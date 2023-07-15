d6804fc5d138beea884474c700bea071638d0b77
RubenTriana <rubendariotriana@gmail.com>
2023-07-15 13:11:14 -0500

    Levanta vista ok, registra Postman ok, envía mailtrap ok

---
+++ app/Mail/SendPost.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Mail;
+use Illuminate\Bus\Queueable;
+use Illuminate\Mail\Mailable;
+use Illuminate\Queue\SerializesModels;
+
+class SendPost extends Mailable
+{
+    use Queueable, SerializesModels;
+
+    public $details;
+    /**
+    * Create a new message instance. *
+    * @return void
+    */
+    public function __construct($details)
+    {
+        $this->details = $details;
+    }
+    /**
+    /**
+    * Build the message. *
+    * @return $this
+    */
+    public function build()
+    {
+        return $this->subject('Curso 2205 - g6')
+            ->view('mail.registroPersona');
+    }
+}
